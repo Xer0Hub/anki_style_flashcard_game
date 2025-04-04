677f14acfcef8e177c3062c2210c11cbaf2b3f0b
Xer0Hub <154411660+Xer0Hub@users.noreply.github.com>
2025-04-04 08:21:35 -0700

    Functionality update
    - Cards can now disappear and French appears after.
    
    - Still need to figure out the sequence of French and English, maybe seperate counts?

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from tkinter import *
 import json as js
 import pandas as pd
+import time
 #---------------------UI SET UP---------------------------#
 #CONSTANTS
 BACKGROUND_COLOR = "#B1DDC6"
@@ -19,12 +20,27 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.geometry("800x500")
 
 #IMAGE LIBRARY
-correct_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\right.png")
-incorrect_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\wrong.png")
-card_front_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\card_front.png")
-card_back_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\card_back.png")
+#AT WORK
+# correct_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\right.png")
+# incorrect_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\wrong.png")
+# card_front_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\card_front.png")
+# card_back_img = PhotoImage(file="C:\\Users\\daniel.ginnane\\PycharmProjects\\Anki_Lite\\card_back.png")
+
+#AT HOME
+correct_img = PhotoImage(file="C:\\Users\\danie\\OneDrive\\Documents\\GitHub\\anki_style_flashcard_game\\right.png")
+incorrect_img = PhotoImage(file="C:\\Users\\danie\\OneDrive\\Documents\\GitHub\\anki_style_flashcard_game\\wrong.png")
+card_front_img = PhotoImage(file="C:\\Users\\danie\\OneDrive\\Documents\\GitHub\\anki_style_flashcard_game\\card_front.png")
+card_back_img = PhotoImage(file="C:\\Users\\danie\\OneDrive\\Documents\\GitHub\\anki_style_flashcard_game\\card_back.png")
+
 
 #--------------------FUNCTIONS AND DEFINITIONS------------------------------#
+def next_question():
+    french_title = Label(text="FRENCH", font=("Arial", 15, "bold"), bg=BACK_OF_CARD)
+    french_title.grid(column=0, row=0, columnspan=2, sticky="n")
+
+    french_card = Label(text=french_dict[card_counter], font=("Arial", 30, "bold"), bg=BACK_OF_CARD)
+    french_card.grid(column=0, row=0, columnspan=2)
+
 def incorrect_answer():
     #KEEP CARD IN DECK
     french_title.grid_remove()
@@ -47,6 +63,8 @@ def correct_answer():
 def show_answer():
     global card_counter
 
+    next_question()
+
     card_front_onscreen = Label(image=card_front_img, bg=BACKGROUND_COLOR, highlightthickness=0, height=300, width=700)
     card_front_onscreen.grid(column=0, row=0, columnspan=2)
 
@@ -55,6 +73,15 @@ def show_answer():
 
     english_card = Label(text=english_dict[card_counter], font=("Arial", 30, "bold"), bg="white")
     english_card.grid(column=0, row=0, columnspan=2)
+
+    english_card.after(3000, english_card.grid_remove)
+    english_title.after(3000, english_title.grid_remove)
+    card_front_onscreen.after(3000, card_front_onscreen.grid_remove)
+
+    card_counter += 1
+
+
+
 
 #---------------------ON SCREEN ------------------------------------#
 #ON SCREEN CARD
@@ -84,8 +111,6 @@ correct_button.grid(column=1, row=1)
 
 incorrect_button = Button(image=incorrect_img, highlightthickness=0, command=incorrect_answer)
 incorrect_button.grid(column=0, row=1)
-
-
 
 
 
